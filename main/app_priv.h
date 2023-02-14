4108d727d315cd59ae499755989286ef0b51d4fa
Jarl Törnroos <jarl.tornroos@gmail.com>
2023-02-14 17:27:05 +0200

    initial commit

---
+++ main/app_priv.h
@@ -0,0 +1,55 @@
+#pragma once
+
+#include <esp_err.h>
+#include <esp_matter.h>
+
+/** Default attribute values used during initialization */
+#define DEFAULT_POWER true
+#define LED_GPIO_PIN GPIO_NUM_32
+
+typedef void *app_driver_handle_t;
+
+/** Initialize the light driver
+ *
+ * This initializes the light driver associated with the selected board.
+ *
+ * @return Handle on success.
+ * @return NULL in case of failure.
+ */
+app_driver_handle_t app_driver_power_init();
+
+/** Initialize the button driver
+ *
+ * This initializes the button driver associated with the selected board.
+ *
+ * @return Handle on success.
+ * @return NULL in case of failure.
+ */
+app_driver_handle_t app_driver_button_init();
+
+/** Driver Update
+ *
+ * This API should be called to update the driver for the attribute being updated.
+ * This is usually called from the common `app_attribute_update_cb()`.
+ *
+ * @param[in] endpoint_id Endpoint ID of the attribute.
+ * @param[in] cluster_id Cluster ID of the attribute.
+ * @param[in] attribute_id Attribute ID of the attribute.
+ * @param[in] val Pointer to `esp_matter_attr_val_t`. Use appropriate elements as per the value type.
+ *
+ * @return ESP_OK on success.
+ * @return error in case of failure.
+ */
+esp_err_t app_driver_attribute_update(app_driver_handle_t driver_handle, uint16_t endpoint_id, uint32_t cluster_id,
+                                      uint32_t attribute_id, esp_matter_attr_val_t *val);
+
+/** Set defaults for power driver
+ *
+ * Set the attribute drivers to their default values from the created data model.
+ *
+ * @param[in] endpoint_id Endpoint ID of the driver.
+ *
+ * @return ESP_OK on success.
+ * @return error in case of failure.
+ */
+esp_err_t app_driver_power_set_defaults(uint16_t endpoint_id);
